268efbecbab14e1b83226d1d7c2491974f2de412
Imaduddin.Haris [ITB-SOE] <P161224004@bussan.co.id>
2025-03-27 00:47:17 +0700

    Update Usage

--- go.mod
+++ go.mod
@@ -1,10 +1,24 @@
 module github.com/duds-fw/dchangelog
 
-go 1.21
+go 1.23.0
+
+toolchain go1.23.4
 
 require (
-	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2
+	github.com/pdfcpu/pdfcpu v0.9.1
+)
+
+require (
+	github.com/hhrutter/lzw v1.0.0 // indirect
+	github.com/hhrutter/tiff v1.0.1 // indirect
+	github.com/mattn/go-runewidth v0.0.16 // indirect
 	github.com/phpdave11/gofpdi v1.0.14-0.20211212211723-1f10f9844311 // indirect
-	github.com/pkg/errors v0.8.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/signintech/gopdf v0.31.0 // indirect
+	golang.org/x/exp v0.0.0-20250305212735-054e65f0b394
+	golang.org/x/image v0.21.0 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,14 +2,61 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"flag"
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
 	"strings"
 	"time"
 
 	"github.com/jung-kurt/gofpdf"
+	"github.com/pdfcpu/pdfcpu/pkg/api"
+	"golang.org/x/exp/slices"
 )
+
+// Config represents the structure of the JSON config file
+type Config struct {
+	PR struct {
+		Link string `json:"link"`
+	} `json:"pr"`
+	Jira struct {
+		Link        string `json:"link"`
+		Title       string `json:"title"`
+		Description string `json:"description"`
+	} `json:"jira"`
+	Developer struct {
+		Name  string `json:"name"`
+		Title string `json:"title"`
+	} `json:"developer"`
+	Project struct {
+		Title string `json:"title"`
+		Value string `json:"value"`
+	} `json:"project"`
+	Status struct {
+		Title string `json:"title"`
+	} `json:"status"`
+	SignApproval struct {
+		Name string `json:"name"`
+		Role string `json:"role"`
+	} `json:"sign_approval"`
+}
+
+// LoadConfig reads the JSON configuration file
+func LoadConfig(filename string) (*Config, error) {
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+
+	var config Config
+	if err := json.Unmarshal(file, &config); err != nil {
+		return nil, err
+	}
+	return &config, nil
+}
 
 // getGitChanges fetches all modified files and their diffs
 func getGitChanges(baseBranch, targetBranch string) (map[string]string, error) {
@@ -41,7 +88,7 @@ func getGitChanges(baseBranch, targetBranch string) (map[string]string, error) {
 }
 
 // generatePDF creates or updates the TSD PDF
-func generatePDF(changes map[string]string) error {
+func generatePDF(config *Config, changes map[string]string) error {
 	pdf := gofpdf.New("P", "mm", "A4", "")
 	pdf.AddPage()
 
@@ -50,11 +97,32 @@ func generatePDF(changes map[string]string) error {
 	pdf.Cell(190, 10, "Technical Specification Document (TSD)")
 	pdf.Ln(15)
 
-	// Developer & Project Info Table
+	// Title
+	pdf.SetFont("Arial", "B", 16)
+	pdf.Cell(190, 10, config.Jira.Title)
+	pdf.Ln(7.5)
+
 	pdf.SetFont("Arial", "", 12)
-	pdf.CellFormat(63, 10, "Developer: John Doe", "1", 0, "C", false, 0, "")
-	pdf.CellFormat(63, 10, "Project: Inventory System", "1", 0, "C", false, 0, "")
-	pdf.CellFormat(64, 10, "Status: In Progress", "1", 1, "C", false, 0, "")
+	pdf.Cell(190, 10, "Link JIRA: "+config.Jira.Link)
+	pdf.Ln(7.5)
+
+	pdf.SetFont("Arial", "", 12)
+	pdf.Cell(190, 10, "Link PR: "+config.PR.Link)
+	pdf.Ln(7.5)
+
+	pdf.SetFont("Arial", "", 12)
+	pdf.Cell(190, 10, "Task Description: "+config.Jira.Description)
+	pdf.Ln(15)
+
+	// Developer & Project Info Table
+	pdf.SetFont("Arial", "B", 12)
+	pdf.CellFormat(63, 10, "Developer", "1", 0, "C", false, 0, "")
+	pdf.CellFormat(63, 10, config.Project.Title, "1", 0, "C", false, 0, "")
+	pdf.CellFormat(64, 10, "Status", "1", 1, "C", false, 0, "")
+	pdf.SetFont("Arial", "", 12)
+	pdf.CellFormat(63, 10, config.Developer.Name, "1", 0, "C", false, 0, "")
+	pdf.CellFormat(63, 10, config.Project.Value, "1", 0, "C", false, 0, "")
+	pdf.CellFormat(64, 10, config.Status.Title, "1", 1, "C", false, 0, "")
 	pdf.Ln(10)
 
 	// Code Changes Table
@@ -89,21 +157,13 @@ func generatePDF(changes map[string]string) error {
 
 	pdf.Ln(10)
 
-	// Empty Query Changes Table
-	pdf.SetFont("Arial", "B", 14)
-	pdf.Cell(190, 10, "Query Changes")
-	pdf.Ln(10)
-	pdf.CellFormat(190, 30, "", "1", 1, "C", false, 0, "")
-	pdf.Ln(10)
-
 	// Sign Approval Table
 	pdf.SetFont("Arial", "B", 14)
 	pdf.Cell(190, 10, "Sign Approval")
 	pdf.Ln(10)
-
 	pdf.SetFont("Arial", "", 12)
-	pdf.CellFormat(95, 10, "Role: Lead Developer", "1", 0, "C", false, 0, "")
-	pdf.CellFormat(95, 10, "Name: John Doe", "1", 1, "C", false, 0, "")
+	pdf.CellFormat(95, 10, "Role: "+config.SignApproval.Role, "1", 0, "C", false, 0, "")
+	pdf.CellFormat(95, 10, "Name: "+config.SignApproval.Name, "1", 1, "C", false, 0, "")
 	pdf.CellFormat(190, 20, "Signature: [Signed]", "1", 1, "C", false, 0, "")
 
 	// Save PDF
@@ -111,28 +171,87 @@ func generatePDF(changes map[string]string) error {
 	return pdf.OutputFileAndClose(fileName)
 }
 
+func mergePDFs(folderPath, outputFileName string) error {
+	files, err := filepath.Glob(filepath.Join(folderPath, "*.pdf"))
+	if err != nil {
+		return err
+	}
+
+	if len(files) == 0 {
+		return fmt.Errorf("no PDF files found in the directory")
+	}
+
+	err = api.MergeCreateFile(files, outputFileName, false, nil)
+	if err != nil {
+		return fmt.Errorf("error merging PDFs: %v", err)
+	}
+
+	fmt.Println("Merged PDF saved as:", outputFileName)
+	return nil
+}
+
 func main() {
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: go run main.go <base_branch> <target_branch>")
-		return
+	usage := []string{"generate", "merge"}
+	args1 := os.Args[1]
+
+	baseBranch := flag.String("dest", "", "Base branch for Git diff")
+	targetBranch := flag.String("src", "", "Target branch for Git diff")
+	folderPath := flag.String("folder", "", "Folder containing PDFs to merge")
+	configFile := flag.String("config", "", "Config json")
+	flag.CommandLine.Parse(os.Args[2:])
+
+	cliScript := "dchangelog"
+
+	// if len(os.Args) < 3 {
+	// 	fmt.Println("Usage:")
+	// 	fmt.Println("  Generate TSD: go run main.go generate <json_file> <base_branch>  <target_branch>")
+	// 	fmt.Println("  Merge PDFs: go run main.go merge <folder_path>")
+	// 	return
+	// }
+
+	if slices.Contains(usage, args1) {
+		if args1 == "generate" {
+
+			if *configFile != "" && *baseBranch != "" && *targetBranch != "" {
+				fmt.Println("Load config ", *configFile, " ...")
+				config, err := LoadConfig(*configFile)
+				if err != nil {
+					log.Fatal("Error loading config:", err)
+					return
+				}
+
+				fmt.Println("Fetching Git changes between", *baseBranch, "and", *targetBranch)
+				changes, err := getGitChanges(*baseBranch, *targetBranch)
+				if err != nil {
+					log.Fatal("Error fetching Git changes:", err)
+					return
+				}
+				fmt.Println("Generating TSD PDF...")
+				err = generatePDF(config, changes)
+				if err != nil {
+					log.Fatal("Error generating PDF:", err)
+					return
+				}
+				fmt.Println("TSD PDF generated successfully!")
+			} else {
+				log.Fatalf("Error Usage\nGenerate TSD: %s generate --config=<jsonfile> --dest=<base_branch> --src=<target_branch>", cliScript)
+			}
+
+		}
+		if args1 == "merge" {
+			if *folderPath != "" {
+				fmt.Println("Merging PDFs in folder:", *folderPath)
+				err := mergePDFs(*folderPath, "merged_output.pdf")
+				if err != nil {
+					fmt.Println("Error merging PDFs:", err)
+				}
+				return
+			} else {
+				log.Fatalf("Error Usage\nMerge PDF: %s merge --folder=<folder> --dest=<base_branch> --src=<target_branch>", cliScript)
+			}
+		}
+	} else {
+		log.Fatalf("Error Usage\nGenerate TSD: %s generate --config=<jsonfile> --dest=<base_branch> --src=<target_branch>\nMerge PDF: %s merge --folder=<folder> --dest=<base_branch> --src=<target_branch>", cliScript, cliScript)
 	}
 
-	baseBranch := os.Args[1]
-	targetBranch := os.Args[2]
-
-	fmt.Println("Fetching Git changes between", baseBranch, "and", targetBranch)
-	changes, err := getGitChanges(baseBranch, targetBranch)
-	if err != nil {
-		fmt.Println("Error fetching Git changes:", err)
-		return
-	}
-
-	fmt.Println("Generating TSD PDF...")
-	err = generatePDF(changes)
-	if err != nil {
-		fmt.Println("Error generating PDF:", err)
-		return
-	}
-
-	fmt.Println("TSD PDF generated successfully!")
 }
